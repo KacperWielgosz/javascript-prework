9bd4fa884b5232115d1bd025fb8b0b1d21ec8770
K W <k4cperwi3lgosz@gmail.com>
2021-12-03 14:14:04 +0100

    Modify script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-function printMessage(msg){
+let computerMove = 'kamień';
+let playerMove = 'papier'
+function printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to playerMove, to wygrywasz!'){
 	let div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
