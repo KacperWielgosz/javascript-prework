e21447303a4e5489cf8668eccf12a0859079afd8
K W <k4cperwi3lgosz@gmail.com>
2021-12-03 14:43:53 +0100

    Add functions to script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,72 @@
-let computerMove = 'kamień';
-let playerMove = 'papier'
-function printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to playerMove, to wygrywasz!'){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+else if(randomNumber == 2){
+  computerMove = 'papier';
+}
+else if(randomNumber == 3){
+  computerMove = 'nożyce';
+}
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+else if(playerInput == '2'){
+  playerMove = 'papier';
+}
+else if(playerInput == '3'){
+  playerMove = 'nożyce';
 }
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
+printMessage('Twój ruch to: ' + playerMove);
+
+if(computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+}
+
+if(computerMove == 'kamień' && playerMove == 'kamień'){
+  printMessage('Remis!');
+}
+
+if(computerMove == 'kamień' && playerMove == 'nożyce'){
+  printMessage('Wygrywam!');
+}
+
+if(computerMove == 'papier' && playerMove == 'nożyce'){
+  printMessage('Ty wygrywasz!');
+}
+
+if(computerMove == 'papier' && playerMove == 'papier'){
+  printMessage('Remis!');
+}
+
+if(computerMove == 'papier' && playerMove == 'kamień'){
+  printMessage('Wygrywam!');
+}
+
+if(computerMove == 'nożyce' && playerMove == 'kamień'){
+  printMessage('Ty wygrywasz!');
+}
+
+if(computerMove == 'nożyce' && playerMove == 'nożyce'){
+  printMessage('Remis!');
+}
+
+if(computerMove == 'nożyce' && playerMove == 'papier'){
+  printMessage('Wygrywam!');
+}
+
+if(playerMove == 'nieznany ruch'){
+  printMessage('Tak sie nie bawię!');
 }
