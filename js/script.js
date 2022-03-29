0ff93ff7bb3b49b1a7c0bcdded8d97fafaca6596
K W <k4cperwi3lgosz@gmail.com>
2022-03-29 23:53:23 +0200

    Put strings into const

--- js/script.js
+++ js/script.js
@@ -1,48 +1,61 @@
 function playgame(playerInput){
   clearMessages();
+
+  const rock = 'kamień';
+  const paper = 'papier';
+  const scisors = 'nożyce';
+  const unnknown = 'nieznany ruch';
+  const win = 'ty wygyrywasz!';
+  const draw = 'remis!';
+  const loose = 'ja wygyrywam!';
+  const yourMove = 'Twój ruch to: ';
+  const iPlayed = 'Zagrałem ';
+  const youPlayed = ', a Ty ';
+  const wrongMove = 'Nie znam ruchu o id ';
+  const incorect = 'Tak sie nie bawię!';
+  const dot = '.';
+  const randomNumber = Math.floor(Math.random() * 3 + 1);
+
   const getMoveName = function (argMoveId) {
-      if (argMoveId == 1) {
-        return 'kamień'
-      } else if (argMoveId == 2) {
-        return 'papier';
-      } else if (argMoveId == 3) {
-        return 'nożyce'
-      }
-      printMessage('Nie znam ruchu o id ' + argMoveId + '.');
-      return 'nieznany ruch';
-    },
-  randomNumber = Math.floor(Math.random() * 3 + 1);
-  console.log('Wylosowana liczba to: ' + randomNumber),
+    if (argMoveId == 1) {
+      return rock;
+    } else if (argMoveId == 2) {
+      return paper;
+    } else if (argMoveId == 3) {
+      return scisors;
+    }
+    printMessage(wrongMove + argMoveId + dot);
+    return unnknown;
+  };
 
   displayResult = function (argComputerMove, argPlayerMove){
     if (
-      (argComputerMove === 'kamień' && argPlayerMove === 'papier')
-      || (argComputerMove === 'papier' && argPlayerMove === 'nożyce')
-      || (argComputerMove === 'nożyce' && argPlayerMove === 'kamień')
+      (argComputerMove === rock && argPlayerMove === paper)
+      || (argComputerMove === paper && argPlayerMove === scisors)
+      || (argComputerMove === scisors && argPlayerMove === rock)
     ) {
-      return'Ty wygrywasz!';
+      return win;
     } else if (
-      (argComputerMove === 'kamień' && argPlayerMove === 'kamień')
-      || (argComputerMove === 'papier' && argPlayerMove === 'papier')
-      || (argComputerMove === 'nożyce' && argPlayerMove === 'nożyce')
+      (argComputerMove === rock && argPlayerMove === rock)
+      || (argComputerMove === paper && argPlayerMove === paper)
+      || (argComputerMove === scisors && argPlayerMove === scisors)
     ) {
-      return('Remis!');
+      return draw;
     } else if (
-      (argComputerMove === 'kamień' && argPlayerMove === 'nożyce')
-      || (argComputerMove === 'papier' && argPlayerMove === 'kamień')
-      || (argComputerMove === 'nożyce' && argPlayerMove === 'papier')
+      (argComputerMove === rock && argPlayerMove === scisors)
+      || (argComputerMove === paper && argPlayerMove === rock)
+      || (argComputerMove === scisors && argPlayerMove === paper)
     ) {
-      return('Wygrywam!');
+      return loose;
     }
-
-    return('Tak sie nie bawię!');
+    return incorect;
   };
 
-  let computerMove = getMoveName(randomNumber);
-      playerMove = getMoveName(playerInput);
+  const computerMove = getMoveName(randomNumber);
+  const playerMove = getMoveName(playerInput);
 
-  printMessage('Twój ruch to: ' + playerMove);
-  printMessage('Zagrałem ' + computerMove + ', a Ty ' + playerMove);
+  printMessage(yourMove + playerMove);
+  printMessage(iPlayed + computerMove + youPlayed + playerMove);
   printMessage(displayResult(computerMove, playerMove))
 }
 
